454516b6cf15d6f89b17d3d0b0f5136444c54e4a
twesleyb <tyler.w.bradshaw@duke.edu>
2019-05-27 20:44:54 -0400

    lazy git commit

---
+++ mutt-setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Set up an app password on your google account.
+# This requires two-factor authentication.
+
+# Create a directory for mutt.
+mkdir -p ~/.mutt/cache
+
+# Create a config file.
+touch ~/.muttrc
---
+++ test-messages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Load utilities, stored in ~/bin which is in our PATH.
+source utils.sh
+
+e_header "I am a sample script"
+e_success "I am a success message"
+e_error "I am an error message"
+e_warning "I am a warning message"
+e_underline "I am underlined text"
+e_bold "I am bold text"
+e_note "I am a note"
